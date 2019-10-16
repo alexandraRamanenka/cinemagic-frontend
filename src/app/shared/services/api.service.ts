24b69a8fcdd82ee91d90d36f431daaadf13f3945
butterflyInACast <butterfly.in.a.cast@gmail.com>
2019-10-16 14:08:33 +0300

    Api service created

---
+++ src/app/shared/services/api.service.ts
@@ -0,0 +1,36 @@
+import { environment } from './../../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  get(path: string, params = new HttpParams()): Observable<any> {
+    return this.http
+      .get(`${environment.url}${path}`, { params })
+      .pipe(catchError((err: any) => throwError(err)));
+  }
+
+  post(path: string, body = {}): Observable<any> {
+    return this.http
+      .post(`${environment.url}${path}`, JSON.stringify(body))
+      .pipe(catchError((err: any) => throwError(err)));
+  }
+
+  patch(path: string, body = {}): Observable<any> {
+    return this.http
+      .patch(`${environment.url}${path}`, JSON.stringify(body))
+      .pipe(catchError((err: any) => throwError(err)));
+  }
+
+  delete(path: string): Observable<any> {
+    return this.http
+      .delete(`${environment.url}${path}`)
+      .pipe(catchError((err: any) => throwError(err)));
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,20 @@
+import { ApiService } from './services/api.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
-  exports: [FormsModule, ReactiveFormsModule, RouterModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HttpClientModule
+  ],
+  providers: [ApiService],
+  exports: [FormsModule, ReactiveFormsModule, RouterModule, HttpClientModule]
 })
 export class SharedModule {}
