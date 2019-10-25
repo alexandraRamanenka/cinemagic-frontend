95f9c477b85861ef1c1db2f98d25dfc2bf720cf2
butterflyInACast <47518853+butterflyInACast@users.noreply.github.com>
2019-10-25 11:23:18 +0300

    Update landing-page.component.ts

--- src/app/modules/landing/components/landing-page/landing-page.component.ts
+++ src/app/modules/landing/components/landing-page/landing-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-  items = [
+  movieSlides = [
     [
       {
         title: '3 hours of concept art',
