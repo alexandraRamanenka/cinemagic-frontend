ee6af1f3567e7e8164cf8a26e738c5f4215faf38
butterflyInACast <butterfly.in.a.cast@gmail.com>
2019-11-11 12:02:12 +0300

    Adding phone pipe

---
+++ src/app/shared/pipes/phone.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'phone'
+})
+export class PhonePipe implements PipeTransform {
+
+  transform(value: any, ...args: any[]): any {
+    return null;
+  }
+
+}
