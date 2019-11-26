9393836d5c0f9606c7fcb1aaddd5fc13082fd6e2
butterflyInACast <butterfly.in.a.cast@gmail.com>
2019-11-26 10:28:07 +0300

    Checkout component

--- src/app/modules/ticket-reservation/components/checkout/checkout.component.ts
+++ src/app/modules/ticket-reservation/components/checkout/checkout.component.ts
@@ -1,3 +1,7 @@
+import { SessionStorageKeys } from '@shared/enums/sessionStorageKeys';
+import { ServiceOrder } from '@shared/models/serviceOrder';
+import { ReservationService } from './../../services/reservation.service';
+import { BlockedSeat } from '@shared/models/blockedSeat';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  seats: BlockedSeat[];
+  services: ServiceOrder[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private reservationService: ReservationService) {
+    this.reservationService.chosenSeats.subscribe(seats => this.seats);
+    this.services = JSON.parse(
+      sessionStorage.getItem(
+        `${this.reservationService.session._id}_${SessionStorageKeys.Services}`
+      )
+    );
   }
 
+  ngOnInit() {}
 }
