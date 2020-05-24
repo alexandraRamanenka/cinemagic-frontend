2d05b300d3eb7b851c7c67885cec103a8e8e42c8
alexandraRamanenka <sasha@form.io>
2020-05-24 13:13:46 +0300

    Darkt themed

--- src/app/modules/afisha/afisha.module.ts
+++ src/app/modules/afisha/afisha.module.ts
@@ -5,7 +5,9 @@ import { AfishaRoutingModule } from './afisha-routing.module';
 import { AfishaPageComponent } from './components/afisha-page/afisha-page.component';
 import { SessionsFiltersComponent } from './components/sessions-filters/sessions-filters.component';
 import { SessionsListComponent } from './components/sessions-list/sessions-list.component';
-import { MatDatepickerModule } from "@angular/material";
+import { MatDatepickerModule, MatFormFieldModule } from "@angular/material";
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,14 @@ import { MatDatepickerModule } from "@angular/material";
     SessionsFiltersComponent,
     SessionsListComponent
   ],
-  imports: [AfishaRoutingModule, SharedModule, MatDatepickerModule],
+  imports: [
+    AfishaRoutingModule,
+    SharedModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   providers: []
 })
 export class AfishaModule {}
